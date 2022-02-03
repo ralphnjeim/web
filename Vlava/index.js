2c0fee97285270401ef1bf8056bbc42cd5c04a3a
ralphnjeim <ralph.b.njeim@gmail.com>
2022-02-03 23:44:22 +0200

    2nd com

---
+++ Vlava/index.js
@@ -0,0 +1,22 @@
+fetch('https://ralphnjm.herokuapp.com/data')
+  .then(response => response.json())
+  .then(json => {
+    var RH = document.getElementById('RH');
+    var OriginalContent = `<div>
+      <div class="icon"><i class="Ralph"></i></div>
+      <h4 class="title"><a href="">FADY</a></h4>
+      <p class="description" >CHRIST</p>
+      </div>`;
+
+    for (var i = 0; i < json.length; i++) {
+      var MyContent = document.createElement('div');
+      var CustomisedContent = OriginalContent;
+      CustomisedContent = CustomisedContent.replace('Ralph', json[i].Icon);
+      CustomisedContent = CustomisedContent.replace('FADY', json[i].Title);
+      CustomisedContent = CustomisedContent.replace('CHRIST', json[i].Description);
+      //CustomisedContent = CustomisedContent.replace('Ralph', json[i].Icon);
+      MyContent.className = 'col-lg-4 col-md-6 icon-box'
+      MyContent.innerHTML = CustomisedContent;
+      RH.appendChild(MyContent);
+    }
+  });
